1ef82589611f99f10e0b45515d39854a6f7ad6f1
Michele 'an4cr0n' Biondi <michelebiondi01@gmail.com>
2018-08-24 17:47:28 +0200

    Increased txpower by 3dB in non smart power mode to reflect DWM1000 Datasheet
    Co-authored-by: Sonic0 <andrea.salvatori92@gmail.com>

--- src/DWM1000.cpp
+++ src/DWM1000.cpp
@@ -549,13 +549,13 @@ void DWM1000Class::txpower() {
 			if(_smartPower) {
 				DWM1000Utils::writeValueToBytes(txpower, 0x15355575L, LEN_TX_POWER);
 			} else {
-				DWM1000Utils::writeValueToBytes(txpower, 0x75757575L, LEN_TX_POWER);
+				DWM1000Utils::writeValueToBytes(txpower, 0x55555555L, LEN_TX_POWER);
 			}
 		} else if(_pulseFrequency == TX_PULSE_FREQ_64MHZ) {
 			if(_smartPower) {
 				DWM1000Utils::writeValueToBytes(txpower, 0x07274767L, LEN_TX_POWER);
 			} else {
-				DWM1000Utils::writeValueToBytes(txpower, 0x67676767L, LEN_TX_POWER);
+				DWM1000Utils::writeValueToBytes(txpower, 0x47474747L, LEN_TX_POWER);
 			}
 		} else {
 			// TODO proper error/warning handling
@@ -565,13 +565,13 @@ void DWM1000Class::txpower() {
 			if(_smartPower) {
 				DWM1000Utils::writeValueToBytes(txpower, 0x0F2F4F6FL, LEN_TX_POWER);
 			} else {
-				DWM1000Utils::writeValueToBytes(txpower, 0x6F6F6F6FL, LEN_TX_POWER);
+				DWM1000Utils::writeValueToBytes(txpower, 0x4F4F4F4FL, LEN_TX_POWER);
 			}
 		} else if(_pulseFrequency == TX_PULSE_FREQ_64MHZ) {
 			if(_smartPower) {
 				DWM1000Utils::writeValueToBytes(txpower, 0x2B4B6B8BL, LEN_TX_POWER);
 			} else {
-				DWM1000Utils::writeValueToBytes(txpower, 0x8B8B8B8BL, LEN_TX_POWER);
+				DWM1000Utils::writeValueToBytes(txpower, 0x6B6B6B6BL, LEN_TX_POWER);
 			}
 		} else {
 			// TODO proper error/warning handling
@@ -581,13 +581,13 @@ void DWM1000Class::txpower() {
 			if(_smartPower) {
 				DWM1000Utils::writeValueToBytes(txpower, 0x1F1F3F5FL, LEN_TX_POWER);
 			} else {
-				DWM1000Utils::writeValueToBytes(txpower, 0x5F5F5F5FL, LEN_TX_POWER);
+				DWM1000Utils::writeValueToBytes(txpower, 0x3F3F3F3FL, LEN_TX_POWER);
 			}
 		} else if(_pulseFrequency == TX_PULSE_FREQ_64MHZ) {
 			if(_smartPower) {
 				DWM1000Utils::writeValueToBytes(txpower, 0x3A5A7A9AL, LEN_TX_POWER);
 			} else {
-				DWM1000Utils::writeValueToBytes(txpower, 0x9A9A9A9AL, LEN_TX_POWER);
+				DWM1000Utils::writeValueToBytes(txpower, 0x7A7A7A7AL, LEN_TX_POWER);
 			}
 		} else {
 			// TODO proper error/warning handling
@@ -597,13 +597,13 @@ void DWM1000Class::txpower() {
 			if(_smartPower) {
 				DWM1000Utils::writeValueToBytes(txpower, 0x0E082848L, LEN_TX_POWER);
 			} else {
-				DWM1000Utils::writeValueToBytes(txpower, 0x48484848L, LEN_TX_POWER);
+				DWM1000Utils::writeValueToBytes(txpower, 0x28282828L, LEN_TX_POWER);
 			}
 		} else if(_pulseFrequency == TX_PULSE_FREQ_64MHZ) {
 			if(_smartPower) {
 				DWM1000Utils::writeValueToBytes(txpower, 0x25456585L, LEN_TX_POWER);
 			} else {
-				DWM1000Utils::writeValueToBytes(txpower, 0x85858585L, LEN_TX_POWER);
+				DWM1000Utils::writeValueToBytes(txpower, 0x65656565L, LEN_TX_POWER);
 			}
 		} else {
 			// TODO proper error/warning handling
@@ -613,13 +613,13 @@ void DWM1000Class::txpower() {
 			if(_smartPower) {
 				DWM1000Utils::writeValueToBytes(txpower, 0x32527292L, LEN_TX_POWER);
 			} else {
-				DWM1000Utils::writeValueToBytes(txpower, 0x92929292L, LEN_TX_POWER);
+				DWM1000Utils::writeValueToBytes(txpower, 0x72727272L, LEN_TX_POWER);
 			}
 		} else if(_pulseFrequency == TX_PULSE_FREQ_64MHZ) {
 			if(_smartPower) {
 				DWM1000Utils::writeValueToBytes(txpower, 0x5171B1D1L, LEN_TX_POWER);
 			} else {
-				DWM1000Utils::writeValueToBytes(txpower, 0xD1D1D1D1L, LEN_TX_POWER);
+				DWM1000Utils::writeValueToBytes(txpower, 0xB1B1B1B1L, LEN_TX_POWER);
 			}
 		} else {
 			// TODO proper error/warning handling
