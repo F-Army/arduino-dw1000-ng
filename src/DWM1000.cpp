f2904bbbd1cc107a95289530fc31635f529095f2
Michele Biondi <michelebiondi01@gmail.com>
2018-08-29 13:49:41 +0200

    Fixed RXPACC Correction based on SFD type (#25)
    Co-authored-by: Sonic0 <andrea.salvatori92@gmail.com>

--- src/DWM1000.cpp
+++ src/DWM1000.cpp
@@ -1661,17 +1661,24 @@ float DWM1000Class::getFirstPathPower() {
 	N_nosat = (uint16_t)rxpacc_nosat[0] | ((uint16_t)rxpacc_nosat[1] << 8);
 	if(N == N_nosat) {
 		/* Needs correction */
+		byte chanCtrl;
 		byte sfdLength;
+		readBytes(CHAN_CTRL, NO_SUB, &chanCtrl, LEN_CHAN_CTRL);
 		readBytes(USR_SFD, SFD_LENGTH_SUB, &sfdLength, LEN_SFD_LENGTH);
-		switch(sfdLength) {
-			case 0x08:
-				N -= 10; break;
-			case 0x10:
-				N -= 18; break;
-			case 0x40:
-				N -= 82; break;
-			default:
-				break;
+		boolean SFD_is_proprietary = DWM1000Utils::getBit(&chanCtrl, LEN_CHAN_CTRL, DWSFD_BIT);
+		if(SFD_is_proprietary) {
+			switch(sfdLength) {
+				case 0x08:
+					N -= 10; break;
+				case 0x10:
+					N -= 18; break;
+				case 0x40:
+					N -= 82; break;
+				default:
+					break;
+			}
+		} else {
+			N -= (sfdLength == 0x08 ? 5 : 64);
 		}
 	}
 
@@ -1702,6 +1709,7 @@ float DWM1000Class::getReceivePower() {
 	readBytes(RX_FINFO, NO_SUB, rxFrameInfo, LEN_RX_FINFO);
 	C = (uint16_t)cirPwrBytes[0] | ((uint16_t)cirPwrBytes[1] << 8);
 	N = (((uint16_t)rxFrameInfo[2] >> 4) & 0xFF) | ((uint16_t)rxFrameInfo[3] << 4);
+	
 	/* Correction of N */
 	byte rxpacc_nosat[LEN_RXPACC_NOSAT];
 	uint16_t N_nosat;
@@ -1709,17 +1717,24 @@ float DWM1000Class::getReceivePower() {
 	N_nosat = (uint16_t)rxpacc_nosat[0] | ((uint16_t)rxpacc_nosat[1] << 8);
 	if(N == N_nosat) {
 		/* Needs correction */
+		byte chanCtrl;
 		byte sfdLength;
+		readBytes(CHAN_CTRL, NO_SUB, &chanCtrl, LEN_CHAN_CTRL);
 		readBytes(USR_SFD, SFD_LENGTH_SUB, &sfdLength, LEN_SFD_LENGTH);
-		switch(sfdLength) {
-			case 0x08:
-				N -= 10; break;
-			case 0x10:
-				N -= 18; break;
-			case 0x40:
-				N -= 82; break;
-			default:
-				break;
+		boolean SFD_is_proprietary = DWM1000Utils::getBit(&chanCtrl, LEN_CHAN_CTRL, DWSFD_BIT);
+		if(SFD_is_proprietary) {
+			switch(sfdLength) {
+				case 0x08:
+					N -= 10; break;
+				case 0x10:
+					N -= 18; break;
+				case 0x40:
+					N -= 82; break;
+				default:
+					break;
+			}
+		} else {
+			N -= (sfdLength == 0x08 ? 5 : 64);
 		}
 	}
 
