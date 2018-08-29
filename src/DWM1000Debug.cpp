0493ca62de60e5d6e93ee59e0b6f43c5bee86241
Michele 'an4cr0n' Biondi <michelebiondi01@gmail.com>
2018-08-29 20:11:51 +0200

    Add copyright notice

--- src/DWM1000Debug.cpp
+++ src/DWM1000Debug.cpp
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2018 by Michele Biondi <michelebiondi01@gmail.com>
+ * Copyright (c) 2015 by Thomas Trojer <thomas@trojer.net>; Arduino-DW1000
+ * Decawave DWM1000 library for arduino.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @file DWM1000Debug.cpp
+ * Debug utilities.
+ */
+
 #include <Arduino.h>
 #include "DWM1000.h"
 
