f790b94d8b932b2abb2883d06eb4f254d6f558e5
Michele 'an4cr0n' Biondi <michelebiondi01@gmail.com>
2018-09-09 14:21:49 +0200

    Changed LICENSE notice inside examples

--- examples/TransmitSpectrumPowerCalibrationTest/TransmitSpectrumPowerCalibrationTest.ino
+++ examples/TransmitSpectrumPowerCalibrationTest/TransmitSpectrumPowerCalibrationTest.ino
@@ -1,22 +1,19 @@
-/**
- * Copyright (c) 2018 Michele 'an4cr0n' Biondi <michelebiondi01@gmail.com> 
- * Copyright (c) 2018 Andrea 'Sonic0' Salvatori <andrea.salvatori92@gmail.com>
- * Decawave DWM1000 driver for arduino.
+/*  
+ *	Arduino-DWM1000 - Arduino library to use Decawave's DWM1000 module.
+ *	Copyright (C) 2018  Michele Biondi <michelebiondi01@gmail.com>, Andrea Salvatori <andrea.salvatori92@gmail.com>
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * @file TransmitSpectrumPowerCalibrationTest.ino
- * This sketch is used for regulatory power testing.
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <SPI.h>
