c196d5b3dadfea9ed4d24ac9da7e31826b47514e
Michele 'an4cr0n' Biondi <michelebiondi01@gmail.com>
2018-09-15 12:29:24 +0200

    Commented out forcetrxfoff

--- src/DWM1000.cpp
+++ src/DWM1000.cpp
@@ -1281,7 +1281,7 @@ namespace DWM1000 {
 	}
 
 	void startTransmit(TransmitMode mode) {
-		forceTRxOff();
+		/* forceTRxOff(); */
 		memset(_sysctrl, 0, LEN_SYS_CTRL);
 		_clearTransmitStatus();
 		_writeTransmitFrameControlRegister();
