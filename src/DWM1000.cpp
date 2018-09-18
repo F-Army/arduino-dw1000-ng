edc401884d946fb6fb48871aaf8cd75d021aad27
Michele 'an4cr0n' Biondi <michelebiondi01@gmail.com>
2018-09-18 16:12:21 +0200

    Add clearReceiveStatus inside startReceive

--- src/DWM1000.cpp
+++ src/DWM1000.cpp
@@ -1305,6 +1305,7 @@ namespace DWM1000 {
 
 	void startReceive(ReceiveMode mode) {
 		memset(_sysctrl, 0, LEN_SYS_CTRL);
+		_clearReceiveStatus();
 		DWM1000Utils::setBit(_sysctrl, LEN_SYS_CTRL, SFCST_BIT, !_frameCheck);
 		if(mode == ReceiveMode::DELAYED)
 			DWM1000Utils::setBit(_sysctrl, LEN_SYS_CTRL, RXDLYS_BIT, true);
