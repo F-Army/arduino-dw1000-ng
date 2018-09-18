0c50701807a733f624178426a618d26b41f8c4b4
Michele 'an4cr0n' Biondi <michelebiondi01@gmail.com>
2018-09-18 18:54:21 +0200

    fix a verdecchia

--- examples/TwoWayRangingInitiator/TwoWayRangingInitiator.ino
+++ examples/TwoWayRangingInitiator/TwoWayRangingInitiator.ino
@@ -121,7 +121,7 @@ void noteActivity() {
 void resetInactive() {
     // tag sends POLL and listens for POLL_ACK
     expectedMsgId = POLL_ACK;
-    DWM1000::forceTRxOff()
+    DWM1000::forceTRxOff();
     transmitPoll();
     noteActivity();
 }
