27292f2ae53d87dc84f0d591361fb3a52f3a1b63
Michele 'an4cr0n' Biondi <michelebiondi01@gmail.com>
2018-09-18 19:13:52 +0200

    Removed receiver autoreenable from example

--- examples/TwoWayRangingInitiator/TwoWayRangingInitiator.ino
+++ examples/TwoWayRangingInitiator/TwoWayRangingInitiator.ino
@@ -91,7 +91,6 @@ void setup() {
     DWM1000::setDefaults();
     DWM1000::setDeviceAddress(2);
     DWM1000::setNetworkId(10);
-    DWM1000::setReceiverAutoReenable(true);
     DWM1000::commitConfiguration();
     Serial.println(F("Committed configuration ..."));
     // DEBUG chip info and registers pretty printed
@@ -108,7 +107,6 @@ void setup() {
     DWM1000::attachSentHandler(handleSent);
     DWM1000::attachReceivedHandler(handleReceived);
     // anchor starts by transmitting a POLL message
-    //DWM1000::receivePermanently(true);
     transmitPoll();
     noteActivity();
 }
