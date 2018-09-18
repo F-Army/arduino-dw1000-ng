eee1337d63bdcde21156c630c50e478d1226bc7c
Michele 'an4cr0n' Biondi <michelebiondi01@gmail.com>
2018-09-18 19:48:16 +0200

    fix

--- examples/SimpleAntennaCalibration/SimpleAntennaCalibration.ino
+++ examples/SimpleAntennaCalibration/SimpleAntennaCalibration.ino
@@ -275,7 +275,6 @@ void loop() {
                 Serial.println(rangeString);
 
                 // Antenna delay script
-                float distance = timeComputedRange.getAsMeters();
                 if(distance >= (EXPECTED_RANGE - EXPECTED_RANGE_EPSILON) && distance <= (EXPECTED_RANGE + EXPECTED_RANGE_EPSILON)) {
                     accuracyCounter++;
                 } else {
