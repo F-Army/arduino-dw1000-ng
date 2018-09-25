adc4c07b6054be2431991f971aa5e75f1b899e5f
Michele 'an4cr0n' Biondi <michelebiondi01@gmail.com>
2018-09-25 19:58:23 +0200

    Removed old license from inappropriate file

--- src/DWM1000Constants.hpp
+++ src/DWM1000Constants.hpp
@@ -16,26 +16,6 @@
  *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/*
- * Copyright (c) 2015 by Thomas Trojer <thomas@trojer.net>
- * Decawave DWM1000 library for arduino.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * @file DWM1000Constants.h
- * Arduino driver library (header file) for the Decawave DWM1000 UWB transceiver module.
- */
-
 #pragma once
 
 #include <Arduino.h>
