9e1a1fb0e30d667fe08d10bdebba1ffd5a7aef60
Michele 'an4cr0n' Biondi <michelebiondi01@gmail.com>
2018-09-25 17:28:11 +0200

    test

--- src/DWM1000.cpp
+++ src/DWM1000.cpp
@@ -1313,8 +1313,10 @@ namespace DWM1000 {
 	}
 
 	void commitConfiguration() {
+		/*
 		if(!_checkPreambleCodeValidity(_preambleCode));
 			_setValidPreambleCode();
+		*/
 		
 		if(!_standardSFD)
 			_setNonStandardSFDLength();
