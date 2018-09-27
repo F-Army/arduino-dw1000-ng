2ea31b895b186ceac913846d3c4d8bf997f5dc42
Michele Biondi <michelebiondi01@gmail.com>
2018-09-27 15:06:40 +0200

    Add DWM1000 Compile optimization (#88)

--- src/DW1000Ng.cpp
+++ src/DW1000Ng.cpp
@@ -373,15 +373,31 @@ namespace DW1000Ng {
 			if(_channel == Channel::CHANNEL_1 || _channel == Channel::CHANNEL_2) {
 				if(_pulseFrequency == PulseFrequency::FREQ_16MHZ) {
 					if(_smartPower) {
+						#if DWM1000_OPTIMIZED
+						DW1000NgUtils::writeValueToBytes(txpower, 0x1B153555L, LEN_TX_POWER);
+						#else
 						DW1000NgUtils::writeValueToBytes(txpower, 0x15355575L, LEN_TX_POWER);
+						#endif
 					} else {
+						#if DWM1000_OPTIMIZED
+						DW1000NgUtils::writeValueToBytes(txpower, 0x55555555L, LEN_TX_POWER);
+						#else
 						DW1000NgUtils::writeValueToBytes(txpower, 0x75757575L, LEN_TX_POWER);
+						#endif
 					}
 				} else if(_pulseFrequency == PulseFrequency::FREQ_64MHZ) {
 					if(_smartPower) {
+						#if DWM1000_OPTIMIZED
+						DW1000NgUtils::writeValueToBytes(txpower, 0x0D072747L, LEN_TX_POWER);
+						#else
 						DW1000NgUtils::writeValueToBytes(txpower, 0x07274767L, LEN_TX_POWER);
+						#endif
 					} else {
+						#if DWM1000_OPTIMIZED
+						DW1000NgUtils::writeValueToBytes(txpower, 0x47474747L, LEN_TX_POWER);
+						#else
 						DW1000NgUtils::writeValueToBytes(txpower, 0x67676767L, LEN_TX_POWER);
+						#endif
 					}
 				} else {
 					// TODO proper error/warning handling
@@ -389,15 +405,31 @@ namespace DW1000Ng {
 			} else if(_channel == Channel::CHANNEL_3) {
 				if(_pulseFrequency == PulseFrequency::FREQ_16MHZ) {
 					if(_smartPower) {
+						#if DWM1000_OPTIMIZED
+						DW1000NgUtils::writeValueToBytes(txpower, 0x150F2F4FL, LEN_TX_POWER);
+						#else
 						DW1000NgUtils::writeValueToBytes(txpower, 0x0F2F4F6FL, LEN_TX_POWER);
+						#endif
 					} else {
+						#if DWM1000_OPTIMIZED
+						DW1000NgUtils::writeValueToBytes(txpower, 0x4F4F4F4FL, LEN_TX_POWER);
+						#else
 						DW1000NgUtils::writeValueToBytes(txpower, 0x6F6F6F6FL, LEN_TX_POWER);
+						#endif
 					}
 				} else if(_pulseFrequency == PulseFrequency::FREQ_64MHZ) {
 					if(_smartPower) {
+						#if DWM1000_OPTIMIZED
+						DW1000NgUtils::writeValueToBytes(txpower, 0x0B2B4B6BL, LEN_TX_POWER);
+						#else
 						DW1000NgUtils::writeValueToBytes(txpower, 0x2B4B6B8BL, LEN_TX_POWER);
+						#endif
 					} else {
+						#if DWM1000_OPTIMIZED
+						DW1000NgUtils::writeValueToBytes(txpower, 0x6B6B6B6BL, LEN_TX_POWER);
+						#else
 						DW1000NgUtils::writeValueToBytes(txpower, 0x8B8B8B8BL, LEN_TX_POWER);
+						#endif
 					}
 				} else {
 					// TODO proper error/warning handling
@@ -405,15 +437,31 @@ namespace DW1000Ng {
 			} else if(_channel == Channel::CHANNEL_4) {
 				if(_pulseFrequency == PulseFrequency::FREQ_16MHZ) {
 					if(_smartPower) {
+						#if DWM1000_OPTIMIZED
+						DW1000NgUtils::writeValueToBytes(txpower, 0x1F1F1F3FL, LEN_TX_POWER);
+						#else
 						DW1000NgUtils::writeValueToBytes(txpower, 0x1F1F3F5FL, LEN_TX_POWER);
+						#endif
 					} else {
+						#if DWM1000_OPTIMIZED
+						DW1000NgUtils::writeValueToBytes(txpower, 0x3F3F3F3FL, LEN_TX_POWER);
+						#else
 						DW1000NgUtils::writeValueToBytes(txpower, 0x5F5F5F5FL, LEN_TX_POWER);
+						#endif
 					}
 				} else if(_pulseFrequency == PulseFrequency::FREQ_64MHZ) {
 					if(_smartPower) {
+						#if DWM1000_OPTIMIZED
+						DW1000NgUtils::writeValueToBytes(txpower, 0x1A3A5A7AL, LEN_TX_POWER);
+						#else
 						DW1000NgUtils::writeValueToBytes(txpower, 0x3A5A7A9AL, LEN_TX_POWER);
+						#endif
 					} else {
+						#if DWM1000_OPTIMIZED
+						DW1000NgUtils::writeValueToBytes(txpower, 0x7A7A7A7AL, LEN_TX_POWER);
+						#else
 						DW1000NgUtils::writeValueToBytes(txpower, 0x9A9A9A9AL, LEN_TX_POWER);
+						#endif
 					}
 				} else {
 					// TODO proper error/warning handling
@@ -421,15 +469,31 @@ namespace DW1000Ng {
 			} else if(_channel == Channel::CHANNEL_5) {
 				if(_pulseFrequency == PulseFrequency::FREQ_16MHZ) {
 					if(_smartPower) {
+						#if DWM1000_OPTIMIZED
+						DW1000NgUtils::writeValueToBytes(txpower, 0x140E0828L, LEN_TX_POWER);
+						#else
 						DW1000NgUtils::writeValueToBytes(txpower, 0x0E082848L, LEN_TX_POWER);
+						#endif
 					} else {
+						#if DWM1000_OPTIMIZED
+						DW1000NgUtils::writeValueToBytes(txpower, 0x28282828L, LEN_TX_POWER);
+						#else
 						DW1000NgUtils::writeValueToBytes(txpower, 0x48484848L, LEN_TX_POWER);
+						#endif
 					}
 				} else if(_pulseFrequency == PulseFrequency::FREQ_64MHZ) {
 					if(_smartPower) {
+						#if DWM1000_OPTIMIZED
+						DW1000NgUtils::writeValueToBytes(txpower, 0x05254565L, LEN_TX_POWER);
+						#else
 						DW1000NgUtils::writeValueToBytes(txpower, 0x25456585L, LEN_TX_POWER);
+						#endif
 					} else {
+						#if DWM1000_OPTIMIZED
+						DW1000NgUtils::writeValueToBytes(txpower, 0x65656565L, LEN_TX_POWER);
+						#else
 						DW1000NgUtils::writeValueToBytes(txpower, 0x85858585L, LEN_TX_POWER);
+						#endif
 					}
 				} else {
 					// TODO proper error/warning handling
@@ -437,15 +501,31 @@ namespace DW1000Ng {
 			} else if(_channel == Channel::CHANNEL_7) {
 				if(_pulseFrequency == PulseFrequency::FREQ_16MHZ) {
 					if(_smartPower) {
+						#if DWM1000_OPTIMIZED
+						DW1000NgUtils::writeValueToBytes(txpower, 0x12325272L, LEN_TX_POWER);
+						#else
 						DW1000NgUtils::writeValueToBytes(txpower, 0x32527292L, LEN_TX_POWER);
+						#endif
 					} else {
+						#if DWM1000_OPTIMIZED
+						DW1000NgUtils::writeValueToBytes(txpower, 0x72727272L, LEN_TX_POWER);
+						#else
 						DW1000NgUtils::writeValueToBytes(txpower, 0x92929292L, LEN_TX_POWER);
+						#endif
 					}
 				} else if(_pulseFrequency == PulseFrequency::FREQ_64MHZ) {
 					if(_smartPower) {
+						#if DWM1000_OPTIMIZED
+						DW1000NgUtils::writeValueToBytes(txpower, 0x315191B1L, LEN_TX_POWER);
+						#else
 						DW1000NgUtils::writeValueToBytes(txpower, 0x5171B1D1L, LEN_TX_POWER);
+						#endif
 					} else {
+						#if DWM1000_OPTIMIZED
+						DW1000NgUtils::writeValueToBytes(txpower, 0xB1B1B1B1L, LEN_TX_POWER);
+						#else
 						DW1000NgUtils::writeValueToBytes(txpower, 0xD1D1D1D1L, LEN_TX_POWER);
+						#endif
 					}
 				} else {
 					// TODO proper error/warning handling
--- src/DW1000NgCompileOptions.hpp
+++ src/DW1000NgCompileOptions.hpp
@@ -1,3 +1,27 @@
+/*
+ * MIT License
+ * 
+ * Copyright (c) 2018 Michele Biondi, Andrea Salvatori
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+*/
+
 /**
  * Copyright (c) 2016 by Ludwig Grill (www.rotzbua.de)
  * Decawave DW1000 library for arduino.
@@ -22,6 +46,11 @@
 #pragma once
 
 /**
+ * Optimizes code for the DWM1000 Module
+ */
+#define DWM1000_OPTIMIZED false
+
+/**
  * Printable DW1000NgTime object costs about: rom: 490 byte ; ram: 58 byte
  * This option is needed because compiler can not optimize unused codes from inheritanced methods 
  * Some examples or debug code use this
@@ -29,7 +58,7 @@
  */
 #define DW1000NGTIME_H_PRINTABLE true
 
-/*
+/**
  * Printable DW1000NgDeviceConfiguration about: rom:2494 byte ; ram 256 byte
  * This option is needed because compiler can not optimize unused codes from inheritanced methods 
  * Some examples or debug code use this
