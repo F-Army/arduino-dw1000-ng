2170bf1d1ea7383c01bc4699fda17908cdab541c
Michele Biondi <michelebiondi01@gmail.com>
2018-10-12 16:53:15 +0200

    Removed copy & paste residual license from other examples

--- examples/StandardTwoWayRangingTag/StandardTwoWayRangingTag.ino
+++ examples/StandardTwoWayRangingTag/StandardTwoWayRangingTag.ino
@@ -22,35 +22,6 @@
  * SOFTWARE.
 */
 
-/*
- * Copyright (c) 2015 by Thomas Trojer <thomas@trojer.net>
- * Decawave DW1000 library for arduino.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * @file RangingTag.ino
- * Use this to test two-way ranging functionality with two DW1000Ng:: This is
- * the tag component's code which polls for range computation. Addressing and
- * frame filtering is currently done in a custom way, as no MAC features are
- * implemented yet.
- *
- * Complements the "RangingAnchor" example sketch.
- *
- * @todo
- *  - use enum instead of define
- *  - move strings to flash (less RAM consumption)
- */
-
 #include <SPI.h>
 #include <DW1000Ng.hpp>
 #include <DW1000NgUtils.hpp>
