5356fa303f8d508783ccca72d9b0598f89b4b1fd
Michele Biondi <michelebiondi01@gmail.com>
2018-11-15 19:25:41 +0100

    test C without delay

--- examples/StandardRTLSAnchorC_TWR/StandardRTLSAnchorC_TWR.ino
+++ examples/StandardRTLSAnchorC_TWR/StandardRTLSAnchorC_TWR.ino
@@ -34,6 +34,8 @@ volatile uint64_t timePollReceived;
 
 volatile double distance;
 
+volatile boolean transmitReportNext = false;
+
 // watchdog and reset period
 volatile uint32_t lastActivity;
 volatile uint32_t resetPeriod = 250;
@@ -165,6 +167,10 @@ void loop() {
 
     if (sentAck) {
         sentAck = false;
+        if(transmitReportNext == true) {
+            transmitReportNext = false;
+            transmitRangeReport();
+        }
         DW1000Ng::startReceive();
     }
 
@@ -187,10 +193,10 @@ void loop() {
             /* In case of wrong read due to bad device calibration */
             if(distance <= 0) 
                 distance = 0.001;
-            
-            delay(3);//Sending message to the DW1000 chip too frequently, the earlier messages won't send out successfully.
+
+            transmitReportNext = true;
             noteActivity();
-            transmitRangeReport();
+            
         }
     }
 }
