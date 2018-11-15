f6d99a8c9c48d02fece81b52ad21104a355cf50c
Michele Biondi <michelebiondi01@gmail.com>
2018-11-15 19:21:25 +0100

    Seq num

--- examples/StandardRTLSAnchorB_TWR/StandardRTLSAnchorB_TWR.ino
+++ examples/StandardRTLSAnchorB_TWR/StandardRTLSAnchorB_TWR.ino
@@ -27,8 +27,6 @@ const uint8_t PIN_SS = SS; // spi select pin
 volatile boolean sentAck = false;
 volatile boolean receivedAck = false;
 
-volatile byte SEQ_NUMBER = 0;
-
 // timestamps to remember
 volatile uint64_t timePollReceived;
 
@@ -146,7 +144,7 @@ void handleReceived() {
 }
 
 void transmitRangeReport() {
-    byte rangingReport[] = {DATA, SHORT_SRC_AND_DEST, SEQ_NUMBER++, 0,0, 0,0, 0,0, 0x60, 0,0 };
+    byte rangingReport[] = {DATA, SHORT_SRC_AND_DEST, DW1000NgRTLS::increaseSequenceNumber(), 0,0, 0,0, 0,0, 0x60, 0,0 };
     DW1000Ng::getNetworkId(&rangingReport[3]);
     memcpy(&rangingReport[5], main_anchor_address, 2);
     DW1000Ng::getDeviceAddress(&rangingReport[7]);
